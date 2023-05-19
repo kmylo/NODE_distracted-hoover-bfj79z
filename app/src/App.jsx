a4f579f58da73e0870f04ca6e80bc3d451d5d46d
kmylo <kmylo@users.noreply.github.com>
2023-05-19 03:26:29 +0000

    Create 12 files add app react + vite + js

---
+++ app/src/App.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+// const EP_URL = "https://localhost:3001/api/notes";
+// const EP_URL = "/api/notes";
+const EP_URL = "https://zhm7xm-3001.csb.app/api/notes";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(EP_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((e) => console.log({ e }));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React!</h1>
+      <ListItems />
+      <pre>
+        DATA:
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
+
+const ListItems = () => {
+  return (
+    <div className="item-list">
+      <div className="item-container">
+        <div className="content">some content</div>
+      </div>
+    </div>
+  );
+};
